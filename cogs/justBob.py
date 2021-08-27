3e345023fa9ea93798868a9bd2543ad3109eb457
adesanjo <jesuisanonime@gmail.com>
2021-08-28 01:37:05 +0200

    corrected typo

--- cogs/justBob.py
+++ cogs/justBob.py
@@ -133,7 +133,7 @@ class JustBob(commands.Cog):
     
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
-        if self.bot.ready and paylod.member is not None and not payload.member.bot and ((payload.member is not None and payload.member.id in self.games and self.games[payload.member.id]["messageId"] == payload.message_id) or (any(role is not None and role.id == UNIT_STAFF for role in payload.member.roles) and payload.emoji.name == STOP)):
+        if self.bot.ready and payload.member is not None and not payload.member.bot and ((payload.member is not None and payload.member.id in self.games and self.games[payload.member.id]["messageId"] == payload.message_id) or (any(role is not None and role.id == UNIT_STAFF for role in payload.member.roles) and payload.emoji.name == STOP)):
             channel = self.bot.get_channel(payload.channel_id)
             game = self.games[payload.member.id]
             gameMessage = await channel.fetch_message(game["messageId"])

f554f2c7104d15e70f0979bc35b9173f19fc0a33
adesanjo <jesuisanonime@gmail.com>
2021-09-14 16:10:24 +0200

    added autodelete flag to stats

--- cogs/schedule.py
+++ cogs/schedule.py
@@ -130,7 +130,8 @@ class Schedule(commands.Cog):
                     "maxPlayers": event["maxPlayers"],
                     "reservableRoles": len(event["reservableRoles"]) if event["reservableRoles"] is not None else 0,
                     "reservedRoles": len([role for role, member in event["reservableRoles"].items() if member is not None]) if event["reservableRoles"] is not None else 0,
-                    "map": event["map"]
+                    "map": event["map"],
+                    "autoDeleted": True
                 }
                 with open(EVENTS_STATS_FILE, "w") as f:
                     json.dump(eventsStats, f, indent=4)
@@ -650,7 +651,8 @@ class Schedule(commands.Cog):
                 "maxPlayers": event["maxPlayers"],
                 "reservableRoles": len(event["reservableRoles"]) if event["reservableRoles"] is not None else 0,
                 "reservedRoles": len([role for role, member in event["reservableRoles"].items() if member is not None]) if event["reservableRoles"] is not None else 0,
-                "map": event["map"]
+                "map": event["map"],
+                "autoDeleted": False
             }
             with open(EVENTS_STATS_FILE, "w") as f:
                 json.dump(eventsStats, f, indent=4)

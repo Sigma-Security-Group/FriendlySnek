8f4fdcfe6243133dc0742d42a0ea6eb6a6ddc45d
adesanjo <jesuisanonime@gmail.com>
2021-09-16 10:29:02 +0200

    added events history

--- anvilStats.py
+++ anvilStats.py
@@ -5,6 +5,7 @@ import anvil.server
 import secret
 
 EVENTS_STATS_FILE = "data/eventsStats.json"
+EVENTS_HISTORY_FILE = "data/eventsHistory.json"
 
 if __name__ == '__main__':
     anvil.server.connect(secret.anvilStatsUplinkKey)
@@ -14,6 +15,12 @@ def getStats():
     with open(EVENTS_STATS_FILE) as f:
         eventsStats = json.load(f)
     return eventsStats
+
+@anvil.server.callable
+def getEventsHistory():
+    with open(EVENTS_HISTORY_FILE) as f:
+        eventsHistory = json.load(f)
+    return eventsHistory
 
 if __name__ == '__main__':
     if not os.path.exists(EVENTS_STATS_FILE):

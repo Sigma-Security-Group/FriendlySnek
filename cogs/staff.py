0c2285c43111000637a45eefbf018bbc36d1da10
adesanjo <jesuisanonime@gmail.com>
2021-10-04 09:22:56 +0200

    y

--- cogs/staff.py
+++ cogs/staff.py
@@ -18,27 +18,6 @@ class Staff(commands.Cog):
     async def on_ready(self):
         log.debug("Staff Cog is ready", flush=True)
         cogsReady["staff"] = True
-        
-        # membersLastMessage = {}
-        # guild = self.bot.get_guild(SERVER)
-        # for member in tqdm(guild.members):
-        #     lastMessage = None
-        #     for channel in tqdm(guild.channels):
-        #         try:
-        #             fetchedMessage = await channel.history().find(lambda m: m.author.id == member.id)
-        #         except AttributeError:
-        #             continue
-        #         if fetchedMessage is None:
-        #             continue
-        #         if lastMessage is None or fetchedMessage.created_at > lastMessage.created_at:
-        #             lastMessage = fetchedMessage
-        #     if lastMessage is not None:
-        #         membersLastMessage[member.display_name] = {"time": str(lastMessage.created_at), "messageId": lastMessage.id, "messageURL": lastMessage.jump_url}
-        #     else:
-        #         print(member.display_name)
-        #         membersLastMessage[member.display_name] = {"time": "NOT FOUNT", "messageId": "NOT FOUNT", "messageURL": "NOT FOUNT"}
-        # with open("data/membersLastMessage.json", "w") as f:
-        #     json.dump(membersLastMessage, f, indent=4)
     
     def getMember(self, searchTerm):
         member = None

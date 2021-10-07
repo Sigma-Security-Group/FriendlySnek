98c3acbf8718cbe18a232990727555e25ddc9e03
adesanjo <jesuisanonime@gmail.com>
2021-10-07 14:23:31 +0200

    added analysis of combat footage and propaganda

--- messageAnalysis.py
+++ messageAnalysis.py
@@ -2,11 +2,15 @@ from constants import *
 
 from __main__ import log, DEBUG
 
+if DEBUG:
+    from constants.debug import *
+
 staffPings = {}
 
 async def runMessageAnalysis(bot, message):
-    pass
     # await staffPingAnalysis(bot, message)
+    await analyzeCombatFootage(bot, message)
+    await analyzePropaganda(bot, message)
 
 async def staffPingAnalysis(bot, message):
     staffRoles = STAFF_ROLES
@@ -25,3 +29,25 @@ async def staffPingAnalysis(bot, message):
         log.debug(f"Staff Pinged\nMessage Link: {message.jump_url}\nMessage Author: {message.author.display_name}({message.author.name}#{message.author.discriminator})")
         botMessage = await bot.get_channel(STAFF_CHAT).send(f"{' '.join(role.mention for role in message.role_mentions if role.id in staffRoles)}: {message.jump_url}")
         staffPings[message.id] = (message, botMessage)
+
+async def analyzeCombatFootage(bot, message):
+    if message.channel.id != COMBAT_FOOTAGE:
+        return
+    if any(role.id == UNIT_STAFF for role in message.author.roles):
+        return
+    if any(attachment.content_type.startswith("video/") for attachment in message.attachments):
+        return
+    if any(videoUrl in message.content for videoUrl in ("://www.youtube.com", "://youtu.be", "://clips.twitch.tv", "://www.twitch.tv", "://streamable.com")):
+        return
+    await message.delete()
+    await message.author.send("The message you just posted in #combat-footage was deleted because no video was detected in it. If this is an error, then please ask staff to post the video for you and inform Adrian about the issue.")
+
+async def analyzePropaganda(bot, message):
+    if message.channel.id != PROPAGANDA:
+        return
+    if any(role.id == UNIT_STAFF for role in message.author.roles):
+        return
+    if any(attachment.content_type.startswith("image/") for attachment in message.attachments):
+        return
+    await message.delete()
+    await message.author.send("The message you just posted in #propaganda was deleted because no image was detected in it. If this is an error, then please ask staff to post the image for you and inform Adrian about the issue.")

7a62708d0196893b5dfe127740ddbdcf56c33b15
Froggi22 <daniel.heinstedt@gmail.com>
2022-05-28 00:41:54 +0200

    MissionUploader server selection
    Upgraded mission uploader a whole lot, with better server implementation and responses.

--- cogs/missionUploader.py
+++ cogs/missionUploader.py
@@ -14,13 +14,24 @@ if DEBUG:
     from constants.debug import *
 
 
-TIMEOUT_EMBED = Embed(title=ERROR_TIMEOUT, color=Color.red())
 MISSIONS_UPLOADED_FILE = "data/missionsUploaded.log"
-# FTP_MISSIONS_DIR = "/144.48.106.194_2316/mpmissions"  # Host Havoc
-#FTP_MISSIONS_DIR = "/euc-ogs7.armahosts.com_2482/mpmissions"  # Dwarf's server
-FTP_MISSIONS_DIR = "/euc-ogs11.armahosts.com_2492/mpmissions"  # Dwarf's server TEST
-
+TIMEOUT_EMBED = Embed(title=ERROR_TIMEOUT, color=Color.red())
 UTC = pytz.utc
+
+SERVERS = [
+    {
+        "Name": "SSG - Operations Server",
+        "Directory": "/euc-ogs7.armahosts.com_2482/mpmissions",
+        "Host": "euc-ogs7.armahosts.com",
+        "Port": 8822
+    },
+    {
+        "Name": "SSG - Training & Testing Server",
+        "Directory": "/euc-ogs11.armahosts.com_2492/mpmissions",
+        "Host": "euc-ogs11.armahosts.com",
+        "Port": 8821
+    }
+]
 
 class MissionUploader(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
@@ -31,7 +42,19 @@ class MissionUploader(commands.Cog):
         log.debug(LOG_COG_READY.format("MissionUploader"), flush=True)
         cogsReady["missionUploader"] = True
 
-    async def checkAttachments(self, dmChannel: discord.DMChannel, attachments: list[discord.Attachment]) -> bool:
+    async def cancelCommand(self, channel: discord.DMChannel, abortText:str) -> None:
+        """ Sends an abort response to the user.
+
+        Parameters:
+        channel (discord.DMChannel): The users DM channel where the message is sent.
+        abortText (str): The embed title - what is aborted.
+
+        Returns:
+        None.
+        """
+        await channel.send(embed=Embed(title=f"❌ {abortText} canceled!", color=Color.red()))
+
+    async def checkAttachments(self, server: dict, dmChannel: discord.DMChannel, attachments: list[discord.Attachment]) -> bool:
         """ Checks a users' message attachemnts if it complies with the set boundries.
 
         Parameters:
@@ -41,11 +64,14 @@ class MissionUploader(commands.Cog):
         Returns:
         bool.
         """
-        with FTP() as ftp:
-            ftp.connect(host=secret.FTP_HOST, port=secret.FTP_PORT)
-            ftp.login(user=secret.FTP_USERNAME, passwd=secret.FTP_PASSWORD)
-            ftp.cwd(FTP_MISSIONS_DIR)
-            missionFilesOnServer = ftp.nlst()
+        try:
+            with FTP() as ftp:
+                ftp.connect(host=server["Host"], port=server["Port"])
+                ftp.login(user=secret.FTP_USERNAME, passwd=secret.FTP_PASSWORD)
+                ftp.cwd(server["Directory"])
+                missionFilesOnServer = ftp.nlst()
+        except Exception as e:
+            print(e)
         attachmentOk = False
         if len(attachments) == 0:
             embed = Embed(title="❌ You didn't upload a file. Please upload the mission file!", color=Color.red())
@@ -80,36 +106,60 @@ class MissionUploader(commands.Cog):
         await interaction.response.send_message("Upload mission file in DMs...")
         log.info(f"{interaction.user.display_name} ({interaction.user.name}#{interaction.user.discriminator}) is uploading a mission file...")
 
-        embed = Embed(title="Upload the mission file you want to put on the server.", color=Color.gold())
-        msg = await interaction.user.send(embed=embed)
-        dmChannel = msg.channel
-        try:
-            response = await self.bot.wait_for("message", timeout=TIME_TEN_MIN, check=lambda msg, interaction=interaction, dmChannel=dmChannel: msg.channel == dmChannel and msg.author == interaction.user)
-            attachments = response.attachments
-            attachmentOk = await self.checkAttachments(dmChannel, attachments)
-        except asyncio.TimeoutError:
-            await dmChannel.send(embed=TIMEOUT_EMBED)
-            return
-        while not attachmentOk:
+        # Server
+        serverSelectOk = False
+        color = Color.gold()
+        while not serverSelectOk:
+            embed = Embed(title="Which server would you like to upload to?", description="\n".join([f"{index}. {server['Name']}" for index, server in enumerate(SERVERS, 1)]), color=color)
+            embed.set_footer(text=SCHEDULE_CANCEL)
+            color = Color.red()
+            msg = await interaction.user.send(embed=embed)
+            dmChannel = msg.channel
             try:
                 response = await self.bot.wait_for("message", timeout=TIME_TEN_MIN, check=lambda msg, interaction=interaction, dmChannel=dmChannel: msg.channel == dmChannel and msg.author == interaction.user)
+                serverNumber = response.content.strip().lower()
+                if serverNumber == "cancel":
+                    await self.cancelCommand(dmChannel, "Mission uploading")
+                    return
+                elif serverNumber in [str(idx[0]) for idx in enumerate(SERVERS, 1)]:
+                    serverSelectOk = True
+                    server = SERVERS[int(serverNumber) - 1]
+            except asyncio.TimeoutError:
+                await dmChannel.send(embed=TIMEOUT_EMBED)
+                return
+
+        # Mission file
+        color = Color.gold()
+        attachmentOk = False
+        while not attachmentOk:
+            embed = Embed(title="Upload the mission file you want to put on the server.", color=color)
+            embed.set_footer(text=SCHEDULE_CANCEL)
+            color = Color.red()
+            msg = await interaction.user.send(embed=embed)
+            dmChannel = msg.channel
+            try:
+                response = await self.bot.wait_for("message", timeout=TIME_TEN_MIN, check=lambda msg, interaction=interaction, dmChannel=dmChannel: msg.channel == dmChannel and msg.author == interaction.user)
+                file = response.content.strip().lower()
+                if file.lower() == "cancel":
+                    await self.cancelCommand(dmChannel, "Mission uploading")
+                    return
                 attachments = response.attachments
-                attachmentOk = await self.checkAttachments(dmChannel, attachments)
+                attachmentOk = await self.checkAttachments(server, dmChannel, attachments)
             except asyncio.TimeoutError:
                 await dmChannel.send(embed=TIMEOUT_EMBED)
                 return
         attachment = attachments[0]
 
-        embed = Embed(title="Uploading mission file...", color=Color.green())
-        await dmChannel.send(embed=embed)
+        # Uploading
+        await dmChannel.send(embed=Embed(title="Uploading mission file...", color=Color.green()))
 
         with open(f"tmp/{attachment.filename}", "wb") as f:
             await attachment.save(f)
 
         with FTP() as ftp:
-            ftp.connect(host=secret.FTP_HOST, port=secret.FTP_PORT)
+            ftp.connect(host=server["Host"], port=server["Port"])
             ftp.login(user=secret.FTP_USERNAME, passwd=secret.FTP_PASSWORD)
-            ftp.cwd(FTP_MISSIONS_DIR)
+            ftp.cwd(server["Directory"])
             if not DEBUG:
                 with open(f"tmp/{attachment.filename}", "rb") as f:
                     ftp.storbinary(f"STOR {attachment.filename}", f)
@@ -117,21 +167,24 @@ class MissionUploader(commands.Cog):
         filename = attachment.filename
         utcTime = UTC.localize(datetime.utcnow())
         member = f"{interaction.user.display_name} ({interaction.user.name}#{interaction.user.discriminator})"
-        memberId = interaction.user.id
 
         with open(MISSIONS_UPLOADED_FILE, "a") as f:
-            f.write(f"\nFilename: {filename}\nUTC Time: {utcTime.strftime(TIME_FORMAT)}\nMember: {member}\nMember ID: {memberId}\n")
+            f.write(f"\nFilename: {filename}\nUTC Time: {utcTime.strftime(TIME_FORMAT)}\nMember: {member}\nMember ID: {interaction.user.id}\n")
 
-        botLogChannel = self.bot.get_channel(BOT)
-        embed = Embed(title="Mission file uploaded!", color=Color.blue())
-        embed.add_field(name="Filename", value=filename)
-        embed.add_field(name="Time", value=utils.format_dt(datetime.utcnow(), style="F"))
-        embed.add_field(name="Member", value=member)
-        embed.add_field(name="Member ID", value=memberId)
-        await botLogChannel.send(embed=embed)
+        embed = Embed(title="Uploaded mission file", color=Color.blue())
+        embed.add_field(name="Filename", value=f"`{filename}`")
+        embed.add_field(name="Server", value=f"`{server['Name']}`")
+        embed.add_field(name="Time", value=utils.format_dt(pytz.timezone("UTC").localize(datetime.utcnow()).astimezone(UTC), style="F"))
+        embed.add_field(name="Member", value=interaction.user.mention)
+        embed.set_footer(text=f"Member ID: {interaction.user.id}")
 
-        log.info(f"{interaction.user.display_name} ({interaction.user.name}#{interaction.user.discriminator}) uploaded a mission file!")
-        embed = Embed(title="Mission file uploaded!", color=Color.green())
+        await self.bot.get_channel(BOT).send(embed=embed)  # Send the log message in the BOT channel
+
+        log.info(f"{interaction.user.display_name} ({interaction.user}) uploaded a mission file!")
+        if not DEBUG:
+            embed = Embed(title="✅ Mission file uploaded", color=Color.green())
+        if DEBUG:
+            embed = Embed(title="Mission file uploaded", description="Actually... The file did not actually upload hehe.\nBot has debug mode enabled!", color=Color.orange())
         await dmChannel.send(embed=embed)
 
     @uploadMission.error

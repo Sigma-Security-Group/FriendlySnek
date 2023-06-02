f5ddf005a455489a98a1ffd94d88a4a5d5caa05e
adesanjo <jesuisanonime@gmail.com>
2023-06-02 20:09:08 +0200

    typo

--- cogs/schedule.py
+++ cogs/schedule.py
@@ -1764,7 +1764,7 @@ class Schedule(commands.Cog):
                 return None
 
             previewEmbed = Embed(title=f":clock3: The starting time has changed for: {event['title']}!", description=f"From: {discord.utils.format_dt(UTC.localize(datetime.strptime(startTimeOld, TIME_FORMAT)), style='F')}\n\u2000\u2000To: {discord.utils.format_dt(UTC.localize(datetime.strptime(event['time'], TIME_FORMAT)), style='F')}", color=Color.orange())
-            previewEmbed.add_field(name="\u200B", value=eventMsg.jump_url}, inline=False)
+            previewEmbed.add_field(name="\u200B", value=eventMsg.jump_url, inline=False)
             for memberId in event["accepted"] + event["declined"] + event["tentative"]:
                 member = guild.get_member(memberId)
                 if member is not None:

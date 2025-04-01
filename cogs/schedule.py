cb24be6b854cc1dc546d01726fb5cf54edc3bb61
Froggi22 <86934446+Froggi22@users.noreply.github.com>
2025-04-01 12:38:26 +0000

    feat: april fools
    I love/hate myself for doing this

--- cogs/schedule.py
+++ cogs/schedule.py
@@ -1,4 +1,5 @@
 import os, re, json, asyncio, discord, logging
+import owo  # april fools
 import pytz  # type: ignore
 
 from math import ceil
@@ -698,7 +699,8 @@ class Schedule(commands.Cog):
         Returns:
         discord.Embed: The generated embed.
         """
-        embed = discord.Embed(title=event["title"], description=event["description"], color=EVENT_TYPE_COLORS[event.get("type", "Operation")])
+        # owo april fools
+        embed = discord.Embed(title=event["title"], description=owo.owo(event["description"]), color=EVENT_TYPE_COLORS[event.get("type", "Operation")])
 
         if event["reservableRoles"] is not None:
             embed.add_field(name="\u200B", value="\u200B", inline=False)
